3f95fa3bbf1f81a7704d29197465c0b9ad12139e
YulietB <di22173@dasc.edu.mx>
2024-05-29 20:34:07 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            if (radioalumno.Checked) // lanza un mensaje de texto si no estan completo los datos y para guardar
+            {
+                MessageBox.Show("Los datos han sido guardado" + "\nNombre:" + textBox1.Text + "\nFecha de nacimiento:" + textBox2.Text 
+                 + "\nEdad:" + textBox3.Text + "\nMatricula: " + textBox4.Text + "\nCarrera: " + textBox5.Text);
+            }
+            else
+            {
+                if (radiodocente.Checked || radioempleado.Checked)
+                {
+                    MessageBox.Show("Los datos han sido guardado" + "\nNombre:" + textBox1.Text + "\nFecha de nacimiento:" + textBox2.Text
+                  + "\nEdad:" + textBox3.Text + "\nDNI: " + textBox4.Text + "\nPuesto: " + textBox5.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Los datos han sido guardado" + "\nNombre:" + textBox1.Text + "\nFecha de nacimiento:" + textBox2.Text
+                 + "\nEdad:" + textBox3.Text);
+                }
+              
+            }
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); //sirve para limpiar la pantalla de cada caja de texto
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();// se hace de tarea
+            // debe de terminar la ejecucion de la interfaz
+        }
+
+        private void validar_Click(object sender, EventArgs e)
+        {
+            if (radioalumno.Checked)
+            {
+                string nombre = textBox1.Text;
+                string fechaN = textBox2.Text;
+                int edad = Convert.ToInt32(textBox3.Text);
+                int matricula = Convert.ToInt32(textBox4.Text);
+                string carrera = textBox5.Text;
+                Alumno alumno = new Alumno(nombre, edad, fechaN, carrera,matricula);
+            }
+            else 
+            {
+                if (radioempleado.Checked)
+                {
+                    string nombre = textBox1.Text;
+                    string fechaN = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                    int matricula = Convert.ToInt32(textBox4.Text);
+                    string rangoejecutivo = textBox5.Text;
+                    float sueldobase = Convert.ToSingle(textBox6.Text);
+                    Trabajador empleado = new Trabajador(nombre, edad, fechaN, rangoejecutivo, sueldobase);
+                }
+
+                else if (radiodocente.Checked)
+                {
+                    string nombre = textBox1.Text;
+                    string fechaN = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                    int matricula = Convert.ToInt32(textBox4.Text);
+                    string rangoejecutivo = textBox5.Text;
+                    float sueldobase = Convert.ToSingle(textBox6.Text);
+                    Maestro docente = new Maestro(nombre, edad, fechaN, rangoejecutivo, sueldobase);
+                }
+                else
+                {
+                    string nombre = textBox1.Text;
+                    string fechaN = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                }
+            
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\proyecto_poop\\parcial2-master\\bienvenida.png";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label4.Visible = false;
+            textBox4.Visible = false;
+            label5.Visible = false;
+            textBox5.Visible = false;
+            label6.Visible = false;
+            textBox6.Visible = false;
+        }
+
+        private void radioempleado_CheckedChanged(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\proyecto_poop\\parcial2-master\\bienvenida.png";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label4.Visible = true;
+            textBox4.Visible = true;
+            label5.Visible = true;
+            textBox5.Visible = true;
+            label6.Visible = true;
+            textBox6.Visible = true;
+        }
+
+        private void radioalumno_CheckedChanged(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\proyecto_poop\\parcial2-master\\bienvenida.png";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label6.Visible = false;
+            textBox6.Visible = false;
+
+        }
+
+        private void radiodocente_CheckedChanged(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\proyecto_poop\\parcial2-master\\bienvenida.png";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label4.Visible = true;
+            textBox4.Visible = true;
+            label5.Visible = true;
+            textBox5.Visible = true;
+            label6.Visible = true;
+            textBox6.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string rutaImagen = "D:\\proyecto_poop\\parcial2-master\\bienvenida.png";
+
+            radioalumno.Checked = false;
+            radioButton1.Checked = false;
+            radiodocente.Checked = false;
+            radioempleado.Checked = false;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    class Persona //clase padre
+    {
+        protected string Nombre; //atributos declaradas para ser usadas mas adelante
+        protected string FechadeN;
+        protected int Edad;
+        protected string RangoEjecutivo;
+
+        public string nombre //Caja de memoria y se ocupa otra nombreclatura en lugar de poner Nombre como se declara se pone nombre por qué si no nos marca error por qué está leyendo lo mismo
+        {
+            get { return Nombre; } //return es para regresar aquí si se ocupa la nomenclatura que ocupamos al inicio
+            set { Nombre = value; } //se le otorga un valor
+        }
+        public string fechaden //caja de memoria
+        {
+            get { return FechadeN; } //return es para regresar
+            set { FechadeN = value; }//se le otorga un valor (el get y set es un constructor)
+        }
+        public int edad
+        {
+            get { return Edad; }
+            set { Edad = value; }
+        }
+        
+        public string rangoejecutivo 
+        {
+            get { return RangoEjecutivo; }
+            set { RangoEjecutivo = value; }
+        }
+//sin la clase persona_herencia no haría la función de leer lo que el el usuario ingresa, aquí solo se declara lo básico que te piden, que normalmente es nuestro nombre, edad, fecha de nacimiento y si eres empleado 
+
+        
+    }
+
+    class Alumno :Persona //clase hija alumno (caja de memoria donde se le da un valor y retorna el valor)
+    {
+        protected int Matricula; //variable
+        protected string Carrera;
+        public int matricula 
+        {
+            get { return Matricula; }
+            set { Matricula = value; }
+        }
+        public string carrera
+        {
+            get { return Carrera; }
+            set { Carrera = value; }
+        }
+        public Alumno(string Nombre,int Edad, string FechadeN, string Carrera, int Matricula)
+        {
+            nombre = Nombre;
+            edad = Edad;    
+            fechaden = FechadeN;
+            matricula = Matricula;
+            carrera = Carrera;
+        }
+    }
+    class Trabajador : Persona //clase hija que se llama empleado y solicita el sueldo que gana y se una una variable int por qué se ingresan valores numéricos 
+    {
+        protected float SueldoBase; //variable
+
+        public float sueldobase
+        {
+            get { return SueldoBase; }
+            set { SueldoBase = value; }
+        }
+        public Trabajador(string Nombre, int Edad, string FechadeN, string RangoEjecutivo, float SueldoBase)
+        {
+            nombre = Nombre;
+            edad = Edad;
+            fechaden = FechadeN;
+            rangoejecutivo = RangoEjecutivo;
+            sueldobase = SueldoBase;
+        }
+     
+    }
+
+    class Maestro: Persona //clase Hija dónde lo que realizar es igual guardar el sueldo que el usuario este ingresando 
+    {
+        protected float SueldoBase;
+        public float sueldobase
+        {
+            get { return SueldoBase; }
+            set { SueldoBase = value; }
+        }
+        public Maestro (string Nombre, int Edad, string FechadeN,string RangoEjecutivo, float SueldoBase)
+        {
+            nombre = Nombre;
+            edad = Edad;
+            fechaden = FechadeN;
+            rangoejecutivo = RangoEjecutivo;
+            sueldobase = SueldoBase;
+        }
+
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
